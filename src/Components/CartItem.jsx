9298c9bbbe816a74453653171b5a4988e7384725
Javier <J_sedanomar@uncg.edu>
2025-06-08 21:45:14 -0400

    Create item and CartItem components

---
+++ src/Components/CartItem.jsx
@@ -0,0 +1,42 @@
+import React, { useState} from 'react'
+
+
+function CartItem({product, onRemoveFromCart}){
+
+const [quantity, setQuantity] = useState(1)
+const [total, setTotal] = useState({product.price})
+
+const Decrement = () => {
+    if(quantity > 1 ){
+      setQuantity(q => q - 1)
+     
+    }
+    
+}
+
+const Increment = () => {
+    setQuantity(q => q + 1)
+    
+}
+
+useEffect(() => {
+    setTotal((quantity * product.price).toFixed(2))
+
+}, [quantity, product.price]);
+
+
+
+    return (
+        <div> 
+            <img className="icon"></img>
+            <p className="item-name"></p>
+            <p className="item-price"></p>
+            <button onClick={decrement}>-</button>
+            <p className="item-quantity">{quantity}</p>
+            <button onClick={increment}>+</button>
+            <p className="item-total">{total}</p>
+            <button onClick={onRemoveFromCar(product.id)}>Remove</button>
+
+        </div>
+    )
+}
---
+++ src/Components/Item.jsx
@@ -0,0 +1,17 @@
+function Item({product, onAddToCart}){
+
+const handleClick = () => {
+    onAddToCart(product)
+}
+
+    return (
+        <div className="item">
+            <img className="item-image">{product.img}</img>
+            <p className="item-name">{product.name}</p>
+            <p className="item-price">{product.price}</p>
+            <button onClick={handleClick}>ADD TO CART</button>
+        </div>
+    );
+}
+
+export default Item;
