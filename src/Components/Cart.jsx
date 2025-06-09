32a6efa31de9b08a0a6998c31bed8c96b1ece997
Javier <J_sedanomar@uncg.edu>
2025-06-09 10:32:26 -0400

    Create Cart Component

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,10 +1,11 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 function Cart(){
 
     const [cart, setCart] = useState([])
+    const [subtotal, setSubtotal] = useState(0)
      
-    const addToCard = (product) => {
+    const addToCart = (product) => {
         setCart(prev => [...prev, product]);
         
     } 
@@ -13,20 +14,34 @@ function Cart(){
         setCart( prev => prev.filter(item => item.id !== idToRemove));
     }
 
+    useEffect(() => {
+        let value = cart.reduce((total, item) => total + item.price, 0)
+        setSubtotal(value)
+    }, [cart])
+
     return(
         <div>
+            <div>
+                <p>Item</p>
+                <p>Price</p>
+                <p>Quantity</p>
+                <p>Total</p>
+            </div>
             {cart.length === 0 ? (
                 <p>Your Cart is Empty</p> 
             ) : (
-            
-                cart.map(product => (
+            <>
+                {cart.map(product => (
                 <CartItem
                 key={product.id}
                 product={product}
                 onRemoveFromCart={removeFromCart}
                 />
-            ))
-            )}
+            ))}
+            <p>Subtotal is {subtotal}</p>
+             </>
+            )
+            }
         
         </div>
     );
