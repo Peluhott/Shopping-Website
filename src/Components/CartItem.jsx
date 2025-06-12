032f211ff5d938b021d324e88594746aa7a4aa73
Javier <J_sedanomar@uncg.edu>
2025-06-12 12:05:22 -0400

    Wrap app in Browser Router, fix minor typos

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -1,34 +1,34 @@
-import React, { useState} from 'react'
+import React, { useState, useEffect } from 'react'
 
 
-function CartItem({product, onRemoveFromCart}){
-    
+function CartItem({ product, onRemoveFromCart }) {
 
-const [quantity, setQuantity] = useState(1)
-const [total, setTotal] = useState({product.price})
 
-const Decrement = () => {
-    if(quantity > 1 ){
-      setQuantity(q => q - 1)
-     
+    const [quantity, setQuantity] = useState(1)
+    const [total, setTotal] = useState(product.price)
+
+    const Decrement = () => {
+        if (quantity > 1) {
+            setQuantity(q => q - 1)
+
+        }
+
     }
-    
-}
 
-const Increment = () => {
-    setQuantity(q => q + 1)
-    
-}
+    const Increment = () => {
+        setQuantity(q => q + 1)
 
-useEffect(() => {
-    setTotal((quantity * product.price).toFixed(2))
+    }
 
-}, [quantity, product.price]);
+    useEffect(() => {
+        setTotal((quantity * product.price).toFixed(2))
+
+    }, [quantity, product.price]);
 
 
 
     return (
-        <div> 
+        <div>
             <img className="icon"></img>
             <p className="item-name"></p>
             <p className="item-price"></p>
--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -1,12 +1,12 @@
-function Item({product, onAddToCart}){
+function Item({ product, onAddToCart }) {
 
-const handleClick = () => {
-    onAddToCart(product)
-}
+    const handleClick = () => {
+        onAddToCart(product)
+    }
 
     return (
         <div className="item">
-            <img className="item-image">{product.image}</img>
+            <img className="item-image" src={product.image} />
             <p className="item-name">{product.name}</p>
             <p className="item-price">{product.price}</p>
             <button onClick={handleClick}>ADD TO CART</button>
