b9c764547917e9e274c21431abaf895cca5b452e
Javier <J_sedanomar@uncg.edu>
2025-06-16 13:32:19 -0400

    finish adding pictures, contact info , form , need to make it media responsive

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
         <div className="home-content">
             <div className="mvp-content">
                 <div className='mpv-image'>
-                    <img src='placeholder.jpeg'/> 
+                    <img className='hero-pic' src='ridingBike.webp'/> 
                     </div>
                 <div className="mvp-text">
                     <h1>Gear Up for New Adventures</h1>
@@ -18,41 +18,69 @@ function Home() {
                 </div>
             </div>
 
-            <div className="video">
-                <div className="video-content">Content for this section</div>
+            <div className="perks">
+                <div className='perk'>
+                    <img className='perk-icon' src='battery.svg'></img>
+                    <p>Long-Lasting Battery</p>
+                </div>
+
+                <div className='perk'>
+                    <img className='perk-icon' src='comfortable.svg'></img>
+                    <p>Built For Comfort</p>
+                </div>
+
+                <div className='perk'>
+                    <img className='perk-icon' src='eco.svg'></img>
+                    <p>Eco-Friendly</p>
+                </div>
             </div>
 
             <div className="featured">
-                <p>Featured Products</p>
+                <h2>Featured Products</h2>
                 <div className="featured-products">
                     <div className="product-cards">
                         <img className='featured-pic' src="/A2.png" alt="product" />
-                        <p>StrideX</p>
+                        <h3>StrideX</h3>
                     </div>
 
                     <div className="product-cards">
                         <img className='featured-pic'  src="/A4.png" alt="product" />
-                        <p>PulseRider</p>
+                        <h3>PulseRider</h3>
                     </div>
 
                     <div className="product-cards">
                         <img className='featured-pic' src="/A5.png" alt="product" />
-                        <p>Cyclone</p>
+                        <h3>Cyclone</h3>
                     </div>
                 </div>
             </div>
 
             <div className="about">
-                <img src="/placeholder.jpeg" alt="about" />
-                <p>Some Bs quote about purpose</p>
+                
+                <p>Ride as much or as little, as long or as short as you feel. But ride.</p>
             </div>
-
-            <div className="contact"></div>
+            <div className='footer'>
+                <div className="contact">
+                <p>Phone: (555)555-5555</p>
+                <p>Location: North Carolina</p>
+                <p>BikeShop@yahoo.com</p>
+            </div>
 
             <div className="sign-up">
-                <p>Sign up to recieve blah blah blah</p>
-                <form>form to sign up</form>
+                <p>Sign up to recieve offers!</p>
+                <form className='sign-up-form'>
+                    <label for='email'>Enter Email</label>
+                    <input
+                    id='email'
+                    type='email'
+                    name='email'
+                    placeholder='Enter your email'
+                    required/>
+                    <button onClick={(e) => {e.preventDefault();}}>Submit</button>
+                </form>
+            </div> 
             </div>
+           
         </div>
     )
 }
